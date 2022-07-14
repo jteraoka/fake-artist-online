282c5c3ab45b9628d5215c186a55340f8b510805
Jared Teraoka <jteraoka13@gmail.com>
2022-07-14 12:56:10 -0700

    edited faq

--- src/public/js/faq-view.vue
+++ src/public/js/faq-view.vue
@@ -14,47 +14,23 @@
 							<p>Join the room again with your username exactly as it was.</p>
 						</div>
 						<div class="qa">
-							<h3>The site is slow / The site went down!</h3>
+							<h3>My browser says my connection is not secure. Should I be concerned?</h3>
 							<p>
-								FAO's currently on Heroku's free hosting service, which has its
-								limitations. One day I'd like to set up a proper server and domain!
-							</p>
-							<p>
-								I also sometimes push updates, usually Mon/Tue, which causes server
-								maintenance. Apologies in advance!
-							</p>
-						</div>
-						<div class="qa">
-							<h3>I found a bug!</h3>
-							<p>
-								I appreciate bug reports! Post an issue on Github, or contact me at
-								<em>fakeartistonline@gmail.com</em>.
+								The reason you are getting this warning is because the site is currently hosted for free on Heroku without a paid SSL certificate.
+								This warning is less "there's a big shark in the water with you, get out" and more "your windows are open, maybe don't walk around naked".
+								As a general rule you should not enter sensitive information on a page with a "Not Secure" warning.
+								Sensitive information includes credit card numbers, full names, date of birth, or addressess.
+								Since we are not entering these things, a "Not secure" connection is safe for the purpose of playing the game.
 							</p>
 						</div>
 						<div class="qa">
-							<h3>Will you implement ___?</h3>
-							<p>
-								FAO is meant to be fairly simple and adaptable for house rules, so
-								I'm not super ambitious. Still, I'm open to suggestions. Make a
-								request on Github, or reach me with the email above.
-							</p>
-						</div>
-						<div class="qa">
-							<h3>Misc. about</h3>
+							<h3>Credits/Disclaimer</h3>
 							<p>FAO is not affiliated with Oink Games.</p>
 							<p>
 								Notification SFX from
 								<a href="https://material.io/design/sound/sound-resources.html"></a>
 								(<a href="https://creativecommons.org/licenses/by/4.0/">CC-BY 4.0</a
 								>)
-							</p>
-							<p>
-								This is a one-man pet project, so I appreciate your patience for
-								fixes and features.
-							</p>
-							<p>
-								Also, this should go without saying, but please play with a modern
-								browser!
 							</p>
 						</div>
 					</div>
